06a878c87aa5433d391129895b8497588106b9cb
AhmedSaied22 <ahmedsaied2016201@gmail.com>
2024-03-20 16:01:49 +0200

    Add search feature view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,11 +1,13 @@
 import 'package:e_book/Features/home/presentation/views/book_details_view.dart';
 import 'package:e_book/Features/home/presentation/views/home_view.dart';
+import 'package:e_book/Features/search/presentation/views/search_view.dart';
 import 'package:e_book/Features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
   static const kBookDetailsView = '/bookDetailsView';
+  static const kSearchView = '/searchView';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -19,6 +21,10 @@ abstract class AppRouter {
       GoRoute(
         path: kBookDetailsView,
         builder: (context, state) => const BookDetailsView(),
+      ),
+      GoRoute(
+        path: kSearchView,
+        builder: (context, state) => const SearchView(),
       ),
     ],
   );
