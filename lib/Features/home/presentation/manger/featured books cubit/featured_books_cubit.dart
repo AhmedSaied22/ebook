824c49b9b340f2ab4e88c1828e48cf2cfd6932c9
AhmedSaied22 <ahmedsaied2016201@gmail.com>
2024-03-23 16:44:25 +0200

    create and finish newest books cubit with its states

--- lib/Features/home/presentation/manger/featured books cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/manger/featured books cubit/featured_books_cubit.dart
@@ -1,4 +1,4 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:e_book/Features/home/data/models/book_model/book_model.dart';
 import 'package:e_book/Features/home/data/repository/home_repo.dart';
 import 'package:equatable/equatable.dart';
